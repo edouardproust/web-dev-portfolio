e045e8eb366fc449e8a968c8241ef88bbd33d877
Edouard Proust <contact@edouardproust.dev>
2022-02-09 19:45:25 +0100

    Added pagination to Project collections

--- src/Controller/CodingLanguageController.php
+++ src/Controller/CodingLanguageController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Config;
 use App\Repository\CodingLanguageRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,21 +15,33 @@ class CodingLanguageController extends AbstractController
     /** @var CodingLanguageRepository */
     private $codingLanguageRepository;
 
+    /** @var PaginatorInterface */
+    private $paginator;
+
     public function __construct(
-        CodingLanguageRepository $codingLanguageRepository
+        CodingLanguageRepository $codingLanguageRepository,
+        PaginatorInterface $paginator
     ) {
         $this->codingLanguageRepository = $codingLanguageRepository;
+        $this->paginator = $paginator;
     }
 
     /**
      * @Route("/projects/language/{slug}", name="coding_language_projects")
      */
-    public function projects($slug): Response
+    public function projects($slug, Request $request): Response
     {
+        $codingLanguage = $this->codingLanguageRepository->findOneBy([
+            'slug' => $slug
+        ]);
+        $projects = $this->paginator->paginate(
+            $codingLanguage->getProjects(),
+            $request->query->getInt('page', 1),
+            Config::POST_PER_PAGE
+        );
         return $this->render('coding_language/projects.html.twig', [
-            'codingLanguage' => $this->codingLanguageRepository->findOneBy([
-                'slug' => $slug
-            ])
+            'codingLanguage' => $codingLanguage,
+            'projects' => $projects
         ]);
     }
 }
--- src/Controller/ProjectCategoryController.php
+++ src/Controller/ProjectCategoryController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
-use App\Repository\ProjectCategoryRepository;
+use App\Config;
 use App\Repository\ProjectRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use App\Repository\ProjectCategoryRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,21 +17,34 @@ class ProjectCategoryController extends AbstractController
     /** @var ProjectCategoryRepository */
     private $projectCategoryRepository;
 
+    /** @var PaginatorInterface */
+    private $paginator;
+
     public function __construct(
-        ProjectCategoryRepository $projectCategoryRepository
+        ProjectCategoryRepository $projectCategoryRepository,
+        PaginatorInterface $paginator
     ) {
         $this->projectCategoryRepository = $projectCategoryRepository;
+        $this->paginator = $paginator;
     }
 
     /**
      * @Route("/projects/category/{slug}", name="project_category")
      */
-    public function index($slug): Response
+    public function index($slug, Request $request): Response
     {
+        $category = $this->projectCategoryRepository->findOneBy([
+            'slug' => $slug
+        ]);
+        $projects = $this->paginator->paginate(
+            $category->getProjects(),
+            $request->query->getInt('page', 1),
+            Config::POST_PER_PAGE
+        );
+
         return $this->render('project_category/index.html.twig', [
-            'category' => $this->projectCategoryRepository->findOneBy([
-                'slug' => $slug
-            ])
+            'category' => $category,
+            'projects' => $projects
         ]);
     }
 }
