58fe87f9347b4d432ef74d27d764d6903e0704e3
Edouard Proust <contact@edouardproust.dev>
2022-04-20 06:06:17 +0200

    pull request

--- assets/admin.js
+++ assets/admin.js
@@ -2,6 +2,8 @@
  * Admin side (made withEasyAdmin Bundle)
  */
 
+console.log('here')
+import CodeBlock from '@ckeditor/ckeditor5-code-block/src/codeblock';
 import shortcodes from './js/admin/shortcodes';
 shortcodes.exec();
 
